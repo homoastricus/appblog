174cf2ea73529ddff614597ba9f42751f87f4d2d
astricus <homoastricus@koreana.spb.ru>
2024-04-23 21:37:22 +0300

    init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 mkdir -p ./logs
-composer update
+#composer update
 
-if [[$1=="dev"]]
-then
-  rm -rf ./config.php
-  mv ./config_dev.php ./config.php
+if [[ $1 == dev ]]; then
+    rm -rf ./config.php
+    mv ./config_dev.php ./config.php
 else
-   rm -rf ./config.php
-   mv ./config_prod.php ./config.php
+    rm -rf ./config.php
+    mv ./config_prod.php ./config.php
 fi
 
 echo 'Production build is success!'
